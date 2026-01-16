3590480278e3fde4f0300b15e8818b0769afe1f2
Gvinu <Gvinu@Vinusha_Mahesh.mshome.net>
2026-01-16 11:20:29 +0530

    added new class in contact module

---
+++ ComcastCRMGUIFrameworkWithTestNG/src/test/java/com/comcast/crm/contacttest/ContactConflict.java
@@ -0,0 +1,12 @@
+package com.comcast.crm.contacttest;
+
+import org.testng.annotations.Test;
+
+public class ContactConflict {
+	@Test
+	public void contactTest()
+	{
+		System.out.println("created new class in contacts module");
+	}
+
+}
